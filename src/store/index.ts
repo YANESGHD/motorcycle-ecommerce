e75cc1221f811c3106714c76becf09d10caaf7e7
Gabriela Yanes <gabriela.yanes@geopagos.com>
2023-07-25 13:36:00 -0300

    chore: remove unused code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,8 @@
 export { store } from './store';
 export type { RootState } from './store';
 export {
-  createMotorcycles,
   modifyMotorcycles,
-  resetMotorcycles,
   fetchingMotorcycles,
   modifyAccessories,
-  resetAccessories,
   fetchingAccessories,
 } from './states';
